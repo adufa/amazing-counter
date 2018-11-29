39a84fd5f3899e070212573c74cb2f0ce94a51d1
Antonio Durán Falero <deadadf@gmail.com>
2018-11-29 14:26:14 +0100

    REalizo el proyecto java

---
+++ contador1/src/paquete/cont.java
@@ -0,0 +1,17 @@
+package paquete;
+
+public class cont {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0;
+	for (int i = 0; i < 101; i++) {
+		num ++;
+		System.out.println(num);
+		
+	}
+				
+
+	}
+
+}
